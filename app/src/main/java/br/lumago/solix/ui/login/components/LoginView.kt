6660f2968e850cda62c88d9f6a1031ce918ae53c
GIOVANA SAGIORATTO <141789329+GiovanaSagioratto@users.noreply.github.com>
2025-10-01 01:02:46 -0300

    Login 70%

--- app/src/main/java/br/lumago/solix/ui/login/components/LoginView.kt
+++ app/src/main/java/br/lumago/solix/ui/login/components/LoginView.kt
@@ -1,46 +1,151 @@
 package br.lumago.solix.ui.login.components
 
-import android.content.Intent
-import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.lumago.solix.ui.payments.PaymentsScreen
-import br.lumago.solix.ui.theme.bgHomeBrush
-import br.lumago.solix.ui.theme.corBotao
-import br.lumago.solix.ui.theme.corGradienteHome1
-import br.lumago.solix.ui.theme.corGradienteHome2
-import br.lumago.solix.ui.theme.titleStyle
 
 
+private val PrimaryColor = Color(0xFF2E473F) // Verde Escuro do Botão
+private val BackgroundStartColor = Color(0xFFC8E6C9)
+private val BackgroundEndColor = Color(0xFF7CB342)
+
+@Preview(showBackground = true)
 @Composable
 fun Login() {
-
-    Icon(
-        painter = rememberVectorPainter(Icons.Default.Person),
-        contentDescription = "user icon",
-        tint = corBotao,
-        modifier = Modifier.size(120.dp)
+    val gradient = Brush.linearGradient(
+        colors = listOf(BackgroundStartColor, BackgroundEndColor),
+        start = Offset(0f, Float.POSITIVE_INFINITY),
+        end = Offset(Float.POSITIVE_INFINITY, 0f)
     )
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(gradient)
+            .padding(16.dp)
+    ) {
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(300.dp)
+                    .wrapContentHeight()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color.White)
+                        .border(8.dp, Color.Black, RoundedCornerShape(16.dp))
+                        .padding(24.dp)
+                        .padding(top = 45.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("E-mail") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Person,
+                                contentDescription = null
+                            )
+                        },
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedContainerColor = Color(0xFFEEEEEE),
+                            unfocusedContainerColor = Color(0xFFEEEEEE)
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Senha") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = null
+                            )
+                        },
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedContainerColor = Color(0xFFEEEEEE),
+                            unfocusedContainerColor = Color(0xFFEEEEEE)
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    Button(
+                        onClick = {
+                            println("Email: $email, Senha: $password")
+                        },
+
+                        colors = ButtonDefaults.buttonColors(containerColor = PrimaryColor),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Entrar", color = Color.White)
+                    }
+
+                    Spacer(modifier = Modifier.height(12.dp))
+                    TextButton(onClick = {  }) {
+                        Text("Não tem uma conta? Cadastre-se", color = Color.Gray)
+                    }
+                }
+
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "User Icon",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .clip(CircleShape)
+                        .background(Color.LightGray)
+                        .border(0.dp, Color.White, CircleShape)
+                        .align(Alignment.TopCenter)
+                        .offset(y = (-45).dp)
+                )
+            }
+        }
     }
+}
